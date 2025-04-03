3490128dd2088100e2a704b574a5e99a408c4800
Zarantech Trainer <ztltrainersap@gmail.com>
2025-04-03 14:58:22 +0000

    first commit

---
+++ test.js
@@ -0,0 +1,40 @@
+console.log("Hello and Welcome to CAP training"); //javascript testing
+var x=10;
+console.log(x); //print on console
+
+//Array with json
+var employeerecords=
+[
+    {
+        "employeename": "Kumud",
+        "emplyeeID": "001",
+        "empDesc": "DM" //myjson
+    },
+    {
+        "employeename": "Mamatha",
+        "emplyeeID": "002",
+        "empDesc": "AM"
+    },
+    {
+        "employeename": "Naveen",
+        "emplyeeID": "003",
+        "empDesc": "Director"
+    }
+];
+
+console.log(employeerecords[1].emplyeeID);
+
+//Types of brackets
+
+//types of brackets
+// curlybrackets : {}
+// parenthesis : ()
+// Array : index []
+
+function number()
+{
+  
+    var val1=10;
+    var val2=20;
+
+}
